557f35234994fd33730eaaf48abf06ee774ed7e7
Mayank <126972040+MayankDevil@users.noreply.github.com>
2023-05-30 18:33:47 +0530

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# website-8
-Entity Code
+# title
+
+- Name : "Entity Code"
+
+- Description : "give code of entity code that you are use in webpage"
+
+- Version : 8.3
+
+- Update : 30-05-2023
+
+- Status : Reset
+
+- Responsive : Ture
+
+- TecStack : { HTML | CSS | JavaScript }
+
+- Thanks : { VSCode | GitHub }
+
+- Work : Self Group
+
+- Design : { Mayank }
+
+- Developer : { Mayank }
+
+- CopyRight : { Mayank }
+
+- URL : https://mayankdevil.github.io/website-8/
+
+- Clone : https://github.com/MayankDevil/website-8.git
+
+- Download : https://github.com/MayankDevil/website-8/archive/refs/heads/main.zip
+
+---
+
+#### Home Page
+
+![Alt text](./data/EntityCode.png "HomePage")
+
+### Responsive
+
+**Breakpoints** { 375 | 576 | 768 | 992 }
+
+### Theme
+
+_ Dark Theme with Orange | Yellow | Gray - White give very nice look to website._
+
+### PreLoader
+
+_it website add pre loader that loader before and it create by code not image_
---
+++ js/script.js
@@ -0,0 +1,51 @@
+/*
+-	website-8 "Entity Code"
+-	CopyRight Mayank All Right Rserved 
+-	Developed and Desgin by Mayank
+-	JavaScript : js/script
+*/
+try
+{
+    /*
+        ===============================
+        | Pre Loader | write and load
+        ===============================
+    */ 
+    let main = document.getElementById('main')
+
+    let loader = document.createElement('div')
+
+    // main.style.display = "none"
+        
+    loader.style.color = "#ff9f43"
+    
+    loader.style.display = "grid"
+
+    loader.style.marginTop = "40vh"
+
+    loader.innerHTML = `<h5> Loader . </h5>`
+
+    var oneSecond = setInterval(() => loader.innerHTML = `<h5> Loading .. </h5>`, 1000);
+    
+    var twoSecond = setInterval(() => loader.innerHTML = `<h5> Please Wait ... </h5>`, 2000);
+    
+    document.body.append(loader)
+
+    window.onload = () => {
+
+        clearInterval(oneSecond)
+
+        clearInterval(twoSecond)
+
+        loader.style.display = "none"
+
+        loader.remove
+
+        main.style.display = ""
+    }
+}
+catch(error)
+{
+    alert(console.error(error));
+}
+// the end
